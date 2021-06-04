a248a4f5fc1419fa3a67129ac849f277c143a0f7
Gabriel Rojas <gabo06123@gmail.com>
2021-06-04 11:40:25 -0400

    refactoring and new things

--- main.py
+++ main.py
@@ -66,47 +66,48 @@ s = requests.Session()
 for cookie in cookies:
   s.cookies.set(cookie['name'], cookie['value'])
 
-try:
-  for k in range(len(links)):
-    browser.get(links[k])
-    time.sleep(1)
-    course = courses[k][0:6]
-    course_name = " ".join(browser.find_element_by_xpath('/html/body/div[2]/div/\
-      h1/span').text.split())
-    course += " - "+course_name
-    try:
-      os.mkdir(f"{PATH}/{course}")
-    except:
-      pass
-    row = browser.find_elements_by_xpath('//*[@id="materiales"]/tbody')
-    for i in range(len(row)):
-      for row2 in row[i].find_elements_by_tag_name('tr'):
-        for row3 in row2.find_elements_by_xpath('td[3]'):
-            row4 = row3.find_elements_by_xpath('h1/a')
-            file_name = " ".join(row4[0].text.split())
-            for j in range(len(file_name)):
-              if file_name[j] == "/":
-                file_name = file_name[:j]+":"+file_name[j+1:]
-            dlink = row4[2].get_attribute('href')[:-8]
-            folder_name = row[i-1].find_element_by_xpath('tr[1]/td[2]').text 
-            if folder_name != "":
-              folder_name += "/"
-              try:
-                os.mkdir(f"{PATH}/{course}/{folder_name}")
-              except:
-                pass
-            r = s.get(dlink)
+for k in range(len(links)):
+  browser.get(links[k])
+  time.sleep(1)
+  course = courses[k][0:6]
+  try:
+    course_name = " ".join(browser.find_element_by_xpath('/html/body/div[2]/\
+      div/h1/span').text.split())
+  except Exception:
+    print(error)
+    print("Wrong username or password")
+    browser.close()
+    sys.exit()
+  course += " - "+course_name
+  try:
+    os.mkdir(f"{PATH}/{course}")
+  except:
+    pass
+  row = browser.find_elements_by_xpath('//*[@id="materiales"]/tbody')
+  for i in range(len(row)):
+    for row2 in row[i].find_elements_by_tag_name('tr'):
+      for row3 in row2.find_elements_by_xpath('td[3]'):
+          row4 = row3.find_elements_by_xpath('h1/a')
+          file_name = " ".join(row4[0].text.split())
+          for j in range(len(file_name)):
+            if file_name[j] == "/":
+              file_name = file_name[:j]+":"+file_name[j+1:]
+          dlink = row4[2].get_attribute('href')[:-8]
+          folder_name = row[i-1].find_element_by_xpath('tr[1]/td[2]').text 
+          if folder_name != "":
+            folder_name += "/"
             try:
-              file = open(f'{PATH}/{course}/{folder_name}{file_name}', 'r+b')
-            except FileNotFoundError:
-              file = open(f'{PATH}/{course}/{folder_name}{file_name}', 'w+b')
+              os.mkdir(f"{PATH}/{course}/{folder_name}")
+            except:
+              pass
+          r = s.get(dlink)
+          try:
+            file = open(f'{PATH}/{course}/{folder_name}{file_name}', 'r+b')
+          except FileNotFoundError:
+            file = open(f'{PATH}/{course}/{folder_name}{file_name}', 'w+b')
 
-            if file.read() != r.content:
-              file.write(r.content)
+          if file.read() != r.content:
+            file.write(r.content)
 
-  browser.close()
+browser.close()
 
-except Exception:
-  print("Wrong username or password")
-  browser.close()
-  sys.exit()
