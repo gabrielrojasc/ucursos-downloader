1a281ab6303dac81a7ea5452355b1a2e03ebbd11
Gabriel Rojas <gabo06123@gmail.com>
2022-11-11 18:42:15 -0300

    --wip-- [skip ci]

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
-import requests
-import os
 import sys
+import os
 from os import path
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from utils import get_semester
-from utils import get_courses
-from utils import get_path
-from utils import get_username
-from utils import get_password
-from utils import get_links
-
-
-def login(browser, wait, username, password):
-    browser.find_element_by_name("username").send_keys(username)
-    wait.until(EC.text_to_be_present_in_element_value((By.NAME, "username"), username))
-    browser.find_element_by_name("password").send_keys(password)
-    wait.until(EC.text_to_be_present_in_element_value((By.NAME, "password"), password))
-    browser.find_element_by_xpath('//*[@id="upform"]/form/dl/input').click()
-    try:
-        wait.until(EC.presence_of_element_located((By.ID, "favoritos")))
-    except:
-        print("Wrong username or password")
-        browser.close()
-        sys.exit()
+from requests_html import HTMLSession
+from utils import (
+    get_courses,
+    get_semester,
+    get_path,
+    get_links,
+    get_password,
+    get_username,
+)
 
 
 def main():
@@ -46,47 +30,42 @@ def main():
 
     links = get_links(courses, semester[0], semester[1])
 
-    browser = webdriver.Safari()
-    wait = WebDriverWait(browser, 5)
-    browser.get("https://www.u-cursos.cl")
-    wait.until(EC.presence_of_element_located((By.NAME, "username")))
-    login(browser, wait, username, password)
+    session = HTMLSession()
 
-    cookies = browser.get_cookies()
-    s = requests.Session()
-    for cookie in cookies:
-        s.cookies.set(cookie["name"], cookie["value"])
+    # login
+    payload = {"username": username, "password": password}
+    session.get("https://www.u-cursos.cl")
+    r = session.post("https://www.u-cursos.cl/upasaporte/adi", data=payload)
+    breakpoint()
+    # TODO: check if login failed
 
     for k in range(len(links)):
-        browser.get(links[k])
-        wait.until(EC.presence_of_element_located((By.ID, "favoritos")))
+        r = session.get(links[k])
+        r.html.render()
+        html = r.html
         course = courses[k][:6]
-        course_name = " ".join(
-            browser.find_element_by_xpath("/html/body/div[2]/div/h1/span").text.split()
-        )
+        breakpoint()
+        course_name = " ".join(html.xpath("/html/body/div[2]/div/h1/span").text.split())
         course += " - " + course_name
         print(f"\n{course}")
-        if not path.exists(f"{PATH}/{course}"):
+        try:
             os.mkdir(f"{PATH}/{course}")
-        row = browser.find_elements_by_xpath('//*[@id="materiales"]/tbody')
+        except FileExistsError:
+            # directory already exists
+            pass
+        row = html.xpath('//*[@id="materiales"]/tbody')
         for i in range(len(row)):
-            for row2 in row[i].find_elements_by_tag_name("tr"):
-                row3_link = row2.find_elements_by_xpath(
-                    'td/h1/a[contains(@class,"baja")]'
-                )
-                row3_name = row2.find_elements_by_xpath(
-                    'td/h1/a[img[contains(@class, "icono")]]'
-                )
+            for row2 in row[i].find("tr"):
+                row3_link = row2.xpath('td/h1/a[contains(@class,"baja")]')
+                row3_name = row2.xpath('td/h1/a[img[contains(@class, "icono")]]')
                 for n in range(len(row3_link)):
-                    file_name = row3_name[n].get_attribute("textContent")
+                    file_name = row3_name[n].attrs.get("textContent")
                     file_name = file_name.replace("\n", "")
                     file_name = file_name.replace("\t", "")
                     file_name = file_name.replace("/", ":")
                     dlink = row3_link[n].get_attribute("href")
                     folder_name = (
-                        row[i - 1]
-                        .find_element_by_xpath('tr[contains(@class,"separador")]')
-                        .text
+                        row[i - 1].xpath('tr[contains(@class,"separador")]').text
                     )
                     folder_name = folder_name.replace("\n", "")
                     folder_name = folder_name.replace("\t", "")
@@ -94,7 +73,7 @@ def main():
                         folder_name += "/"
                         if not path.exists(f"{PATH}/{course}/{folder_name}"):
                             os.mkdir(f"{PATH}/{course}/{folder_name}")
-                    r = s.get(dlink)
+                    r = session.get(dlink)
                     if path.exists(f"{PATH}/{course}/{folder_name}{file_name}"):
                         print(f"Updating: {file_name}")
                     else:
@@ -102,9 +81,6 @@ def main():
                     file = open(f"{PATH}/{course}/{folder_name}{file_name}", "w+b")
                     file.write(r.content)
                     file.close()
-
-    browser.close()
-    s.close()
 
 
 if __name__ == "__main__":
